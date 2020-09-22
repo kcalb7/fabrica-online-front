77b218a5435c99336eccc96c16c430110a995a67
douglas <douglasbraga_lp@hotmail.com>
2020-09-21 23:37:26 -0300

    add vuetify and basic configuration on routes and components

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/contas",
+		name: "Contas",
+		component: () => import("../views/Contas.vue"),
+	},
+	{
+		path: "/relatorios",
+		name: "Relatorios",
+		component: () => import("../views/Relatorios.vue"),
+	},
+	{
+		path: "/usuarios",
+		name: "Usuarios",
+		component: () => import("../views/Usuarios.vue"),
+	},
+	{
+		path: "/about",
+		name: "About",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/About.vue"),
+	},
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Usuarios.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1>Componente USUÁRIOS</h1>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
